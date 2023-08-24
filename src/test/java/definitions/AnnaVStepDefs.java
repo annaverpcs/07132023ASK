c55c5579ea5135b5fe4d79816a67e8c87f77eefe
Anna Verkhovskaya <annaverpcs@gmail.com>
2023-08-23 18:45:36 -0700

    AnnaV:src/test/java/definitions/AnnaVStepDefs.java

---
+++ src/test/java/definitions/AnnaVStepDefs.java
@@ -0,0 +1,26 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class AnnaVStepDefs {
+    @Then("AV retrieve information from db for email {string}")
+    public void avRetrieveInformationFromDbForEmail(String arg0) {
+    }
+
+    @Then("AV activate the user")
+    public void avActivateTheUser() {
+    }
+
+    @Given("AV open page {string}")
+    public void avOpenPage(String arg0) {
+    }
+
+    @Then("AV type {string} to field {string}")
+    public void avTypeToField(String arg0, String arg1) {
+    }
+
+    @Then("AV click on button {string}")
+    public void avClickOnButton(String arg0) {
+    }
+}
